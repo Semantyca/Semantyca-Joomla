36a232bca324430fcd2d158e768f0e8549384911
aida <aidazimas@hotmail.com>
2024-03-14 23:16:07 +0200

    #

--- com_semantycanm/admin/assets/js/src/utils/msgGen.js
+++ com_semantycanm/admin/assets/js/src/utils/msgGen.js
@@ -19,11 +19,7 @@ const normalizeUrl = (url) => {
 export const getWrappedContent = (content) => {
     const templateStore = useTemplateStore();
     const data = {content};
-    const wrapperTemplateString = templateStore.doc.wrapper
-        .replace(/&lt;%/g, '<%')
-        .replace(/%&gt;/g, '%>');
-    //const wrapperTemplateString =  templateStore.doc.wrapper;
-    return ejs.render(wrapperTemplateString, data);
+    return ejs.render(templateStore.doc.wrapper, data);
 };
 
 export const buildContent = (currentDateFormatted, currentYear, selectedArticles) => {
@@ -44,35 +40,5 @@ export const buildContent = (currentDateFormatted, currentYear, selectedArticles
         maxArticles: templateStore.doc.maxArticles,
         maxArticlesShort: templateStore.doc.maxArticlesShort,
     };
-
-
-    const template = '<p>Hello, <%= currentDateFormatted %>!</p>';
-    //   const data1 = { name: 'World' };
-    //  const output = ejs.render(template, data1);
-    //   console.log('test', output); //
-    const templateString = decodeEJSTemplate(templateStore.doc.html);
-
-    // const templateString =  templateStore.doc.html;
-    console.log('template', templateString);
-
-    let r = ejs.render(templateString, data);
-    console.log(r);
-    return r;
+    return ejs.render(templateStore.doc.html, data);
 };
-
-function decodeEJSTemplate(templateString) {
-    // Step 1: Decode EJS tags
-    let decodedString = templateString
-        .replace(/&lt;%/g, '<%')
-        .replace(/%&gt;/g, '%>');
-
-    // Step 2: Decode common logical operators within EJS tags
-    // This includes <, >, and & within loops and conditionals
-    // Note: This regex is simplified for demonstration and might need adjustments
-    decodedString = decodedString.replace(/(<%[^%>]*?)&amp;([^%>]*?%>)/g, '$1&$2')
-        .replace(/(<%[^%>]*?)&lt;([^%>]*?%>)/g, '$1<$2')
-        .replace(/(<%[^%>]*?)&gt;([^%>]*?%>)/g, '$1>$2');
-
-    return decodedString;
-}
-
