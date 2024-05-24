54dae8e3ad203002f9eb0b1920e734fe0a3dc4e5
aida <aidazimas@hotmail.com>
2024-05-24 21:06:37 +0100

    Added logging messages to install, update, uninstall, preflight, postflight, and directory cleanup functions in installer_script.php

--- com_semantycanm/admin/installer_script.php
+++ com_semantycanm/admin/installer_script.php
@@ -5,48 +5,67 @@ class Com_SemantycanmInstallerScript
 {
 	public function install($parent)
 	{
+		$this->logMessage('Install: Cleaning bundle directory...');
 		$this->cleanBundleDirectory();
 	}
 
 	public function update($parent)
 	{
+		$this->logMessage('Update: Cleaning bundle directory...');
 		$this->cleanBundleDirectory();
 	}
 
 	public function uninstall($parent)
 	{
-
+		$this->logMessage('Uninstall: No actions taken.');
 	}
 
 	public function preflight($type, $parent)
 	{
-
+		$this->logMessage("Preflight ({$type}): No actions taken.");
 	}
 
 	public function postflight($type, $parent)
 	{
-
+		$this->logMessage("Postflight ({$type}): No actions taken.");
 	}
 
 	private function cleanBundleDirectory()
 	{
 		$bundleDir = JPATH_ADMINISTRATOR . '/components/com_semantycanm/assets/bundle';
-		if (is_dir($bundleDir)) {
+		if (is_dir($bundleDir))
+		{
+			$this->logMessage("Cleaning directory: {$bundleDir}");
 			$this->deleteDirectoryContents($bundleDir);
+		}
+		else
+		{
+			$this->logMessage("Directory does not exist: {$bundleDir}");
 		}
 	}
 
 	private function deleteDirectoryContents($dir)
 	{
 		$files = array_diff(scandir($dir), array('.', '..'));
-		foreach ($files as $file) {
+		foreach ($files as $file)
+		{
 			$path = $dir . '/' . $file;
-			if (is_dir($path)) {
+			if (is_dir($path))
+			{
 				$this->deleteDirectoryContents($path);
 				rmdir($path);
-			} else {
+				$this->logMessage("Removed directory: {$path}");
+			}
+			else
+			{
 				unlink($path);
+				$this->logMessage("Removed file: {$path}");
 			}
 		}
+	}
+
+	private function logMessage($message)
+	{
+		JLog::add($message, JLog::INFO, 'com_semantycanm');
 	}
 }
