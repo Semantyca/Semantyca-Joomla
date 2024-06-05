61b0336e55d16247a44a0bf398a43836c9fe335c
kaira <justaidajam@gmail.com>
2024-06-05 09:31:23 +0100

    Updated version, refined message text and minor settings changes in Semantyca NM

--- com_semantycanm/admin/src/View/Dashboard/HtmlView.php
+++ com_semantycanm/admin/src/View/Dashboard/HtmlView.php
@@ -7,6 +7,7 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Toolbar\ToolbarHelper;
+use JText;
 
 class HtmlView extends BaseHtmlView
 {
@@ -17,11 +18,16 @@ class HtmlView extends BaseHtmlView
 
 	protected function addToolbar()
 	{
-		$user = Factory::getUser();
+		$lang = Factory::getApplication()->getLanguage();
+		$lang->load('com_semantycanm', JPATH_ADMINISTRATOR) || $lang->load('com_semantycanm', JPATH_SITE);
+
+		$user = Factory::getApplication()->getIdentity();
 
 		if ($user->authorise('core.admin', 'com_semantycanm'))
 		{
 			ToolbarHelper::preferences('com_semantycanm');
 		}
+
+		ToolbarHelper::title(JText::_('COM_SEMANTYCANM_CONFIGURATION'), 'icon-semantic');
 	}
 }
